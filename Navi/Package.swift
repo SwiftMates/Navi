c4ee2023acc20d133f24683ad35b8df85b7f2c71
Lazar-Kiss Mark <lkmark94@gmail.com>
2026-03-28 17:41:02 +0200

    style: increment swift version and minim target

--- Navi/Package.swift
+++ Navi/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Navi",
-    platforms: [.iOS(.v16), .macOS(.v13)],
+    platforms: [.iOS(.v17), .macOS(.v14)],
     products: [
         .library(
             name: "Navi",
