2f6e916739dc3c07fd8677b4c597a06dc5041393
David Pall <davidpallatx@gmail.com>
2026-07-13 19:37:05 +0200

    feat: - Demo app initialised

---
+++ Examples/Coordinators/Coordinators/CoordinatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorsApp.swift
+//  Coordinators
+//
+//  Created by David Pall on 2026. 07. 13..
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
