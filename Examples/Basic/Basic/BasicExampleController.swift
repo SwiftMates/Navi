a6babda4ab215ce6732c1ed455f59b4767886da3
David Pall <davidpallatx@gmail.com>
2026-07-13 19:16:55 +0200

    feat: - Basic logger moved into separate file

--- Examples/Basic/Basic/BasicExampleController.swift
+++ Examples/Basic/Basic/BasicExampleController.swift
@@ -7,21 +7,8 @@
 
 import Foundation
 import Navi
-import OSLog
 
 @Observable
 final class BasicExampleController: NaviController {
     var properties = NaviControllerProperties(logger: BasicLogger())
-}
-
-final class BasicLogger: NaviLoggerable {
-    let logger = Logger(subsystem: "com.navi.example.basic", category: "Navi")
-    
-    func logInfo(_ message: String) {
-        logger.info("\(message, privacy: .public)")
-    }
-    
-    func logError(_ message: String) {
-        logger.error("\(message, privacy: .public)")
-    }
 }
