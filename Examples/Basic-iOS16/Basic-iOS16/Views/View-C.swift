de14eedf3eaa803b929df38d33636b0813ce2b6a
Mark Lazar-Kiss <89514035+rEdh1t94@users.noreply.github.com>
2026-08-31 15:54:33 +0300

    feat: lower minim ios version to 16 (#39)

---
+++ Examples/Basic-iOS16/Basic-iOS16/Views/View-C.swift
@@ -0,0 +1,31 @@
+//
+//  View-C.swift
+//  Basic
+//
+//  Created by David Pall on 2026. 07. 07..
+//
+
+import SwiftUI
+import Navi
+
+struct ViewC: View {
+    
+    @EnvironmentObject var controller: BasicExampleController
+    
+    var body: some View {
+        VStack(spacing: 24) {
+            Button("Pop to View - A") {
+                controller.pop(to: MainViewsDestinations.Origins.viewA)
+            }
+            
+            Button("Pop") {
+                controller.pop()
+            }
+            
+            Button("Pop to root") {
+                controller.popToRoot()
+            }
+        }
+        .navigationTitle("View - C")
+    }
+}
