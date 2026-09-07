4e4dbd265b9ea50e62ddd6fa088e60ecf1fef7f9
David Pall <davidpallatx@gmail.com>
2026-09-07 20:43:46 +0200

    docs: Todo added

--- Examples/Coordinators/Coordinators/Presentation/A-Flow/A-FlowCoordinator.swift
+++ Examples/Coordinators/Coordinators/Presentation/A-Flow/A-FlowCoordinator.swift
@@ -17,6 +17,7 @@ struct AFlowCoordinator {
         @OriginKey case stepThree
     }
     
+    // TODO: - Discuss if we should merge all destinations into one Enum (screens and other coordinators togehter)
     @DestinationRepresentable
     enum Coordinators {
         case bFlow
