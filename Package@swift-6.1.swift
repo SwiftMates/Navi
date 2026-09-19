813f06128bdc7f90dac76b50d80feac3e1415d7c
Lazar-Kiss Mark <lkmark94@gmail.com>
2026-09-19 22:10:17 +0300

    feat: migrate Navi and examples to Swift 6.4 and xcode 27

--- Package@swift-6.1.swift
+++ Package@swift-6.1.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax", .upToNextMajor(from: "604.0.0"))
+        .package(url: "https://github.com/apple/swift-syntax", .upToNextMajor(from: "601.0.0"))
     ],
     targets: [
         .macro(
